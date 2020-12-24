a8846d97e766ba67b2ca685446c2b4e2e0a39e37
brightwing1218 <74066981+brightwing1218@users.noreply.github.com>
2020-12-24 14:01:50 +0900

    Add files via upload

---
+++ Watson.py
@@ -0,0 +1,15 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready(): #봇이 구동할 준비가 되었을 때 하단 명령 실행
+    print(client.user.id)
+    print("ready")
+
+@client.event
+async def on_message(message):#누군가 메시지를 보냈을 때 하단 명령 실행
+    if message.content.startswith("안녕"):
+        await message.channel.send("반가워")
+
+client.run("NzkxNTI0NzIzMDA3Njg0NzA4.X-Qa1w.wTlrz3CFNdJEzNX14YgkH0K3uO0")
