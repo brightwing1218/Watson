3c0a9255bc3cc605bd5aeee5c654e0eaa95ef012
brightwing1218 <74066981+brightwing1218@users.noreply.github.com>
2020-12-25 20:08:07 +0900

    Update Watson.py

--- Watson.py
+++ Watson.py
@@ -1,4 +1,5 @@
 import discord
+import os
 
 client = discord.Client()
 
@@ -12,4 +13,6 @@ async def on_message(message):#누군가 메시지를 보냈을 때 하단 명�
     if message.content.startswith("안녕"):
         await message.channel.send("반가워")
 
-client.run("NzkxNTI0NzIzMDA3Njg0NzA4.X-Qa1w.rR0LS4vtmYX19dBvDBMg4TdINzY")
+
+access_token = os.environ["BOT_TOKEN"]        
+client.run(access_token)
